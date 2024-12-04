e6cdece8c3876d545e8f193f4994093d4d6f8ef8
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-12-04 17:02:18 +0700

    base frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,45 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import AuthLayout from './pages/Auth'
+import SignInPage from './pages/Auth/SignIn'
+import SignUpPage from './pages/Auth/SignUp'
+import RootLayout from './pages/Root'
+import HomePage from './pages/Root/Home'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      }
+    ]
+  },
+  {
+    path: '/sign-in',
+    element: <AuthLayout />,
+    children: [
+      {
+        index: true,
+        element: <SignInPage />
+      }
+    ]
+  },
+  {
+    path: '/sign-up',
+    element: <AuthLayout />,
+    children: [
+      {
+        index: true,
+        element: <SignUpPage />
+      }
+    ]
+  }
+])
+
 const App = () => {
-  return <div className='bg-red-50'>App</div>
+  return <RouterProvider router={router} />
 }
 
 export default App
