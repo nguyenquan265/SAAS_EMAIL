2adb2b9e494ece07d2b48e662776682ebf4f7af1
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-12-08 01:00:00 +0700

    thay doi vi tri cua authObject

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,12 +1,5 @@
 import { NextFunction, Request, Response } from 'express'
-import { AuthObject } from '@clerk/express'
 import ApiError from '../utils/ApiError'
-
-declare module 'express' {
-  interface Request {
-    auth?: AuthObject
-  }
-}
 
 export const authenticate = (req: Request, res: Response, next: NextFunction) => {
   if (!req.auth || !req.auth.userId) {
